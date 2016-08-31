2623508bd0547bc14a2bd33b91e6034b96bbb607
Adi Kancherla <akancherla@apigee.com>
2016-08-30 18:07:42 -0700

    login in progress

--- index.ios.js
+++ index.ios.js
@@ -2,3 +2,15 @@ import {AppRegistry} from 'react-native';
 const index = require('./components/index/index');
 
 AppRegistry.registerComponent('Videoness', index);
+            
+
+/*import {React, Text, AppRegistry} from 'react-native';
+//const index = require('./components/index/index');
+var tmp = React.createClass({
+  render(){
+    return (
+      <Text>yoyo</Text>
+    );
+  }
+});
+AppRegistry.registerComponent('Videoness', () => tmp);*/
